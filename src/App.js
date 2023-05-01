73a6d9142853ffa360b89a0eb2ae75cea900c224
OtabekTohirovich <tohirovo630@gmail.com>
2023-05-01 23:52:53 +0500

    Progress UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Main from "./layout/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main className="content">
+        {/* <NavbarMain showDrawer={showDrawer} open={open} /> */}
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/* <Route path="/delivery" element={<Delivery />} />
+          <Route path="/purchases" element={<Purchases />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </main>
+      {/* <SidebarMenu showDrawer={showDrawer} open={open} onClose={onClose} /> */}
     </div>
   );
 }
---
+++ src/layout/main.jsx
@@ -0,0 +1,14 @@
+import { Button, Container } from '@mui/material'
+import React from 'react'
+
+const Main = () => {
+  return (
+    <Container maxWidth="1300" fixed>
+        <Button>
+          We want to do 
+        </Button>
+    </Container>
+  )
+}
+
+export default Main
